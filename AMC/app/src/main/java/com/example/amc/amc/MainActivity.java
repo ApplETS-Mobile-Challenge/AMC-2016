b6d07df469a94c2640ece3d7d834757c634bf534
Michaël Légaré <gragor30@gmail.com>
2016-02-13 03:13:28 -0500

    ajout du menu

--- AMC/app/src/main/java/com/example/amc/amc/MainActivity.java
+++ AMC/app/src/main/java/com/example/amc/amc/MainActivity.java
@@ -98,17 +98,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_connect) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_deconnect) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_info) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_MAnnonces) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_LAnnonces) {
 
         }
 
